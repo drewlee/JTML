7745a4f751943bf15855b97ffa881bf0c957344b
Andrew Lee <starsthatfell@gmail.com>
2011-12-29 03:24:31 -0600

    added tests

--- JHTML.js
+++ JHTML.js
@@ -1,4 +1,4 @@
-(function(w){
+;(function(w){
 	// quit if function name already exists
 	if(typeof JHTMLparse !== 'undefined'){return;}
 	
@@ -31,11 +31,11 @@
 		
 		// if string
 		}else if(typeof obj == 'string'){
-			str += obj + '\n';
+			str += obj;
 			
 		// else some invalid value
 		}else{
-			str += '\n';
+			str += '';
 		}
         
         return str;
@@ -43,34 +43,41 @@
 	
 	// parse 
 	function parseAttr(selector){
-		var single   = /input|meta|col|br|hr/,
-			isSingle = false,
-			arr  = [],
-			tag  = selector.match(/^[^#.[\]]+/),
-			id   = selector.match(/#([^.\[]+)/),
-			cls  = selector.match(/\.([^#.\[])+/g),
-			attr = selector.match(/(\[.[^\]]+\])/g);
+		var arr = [],
+			single = /^(input|meta|col|br|hr|img|link)$/i,
+			isSingle = false;
 		
+		// match attributes and remove them
+		var aReg = /\[[^\]]+\]/g,
+			attr = selector.match(aReg);
+		if(attr){
+			attr = attr.join(' ');
+			attr = attr.replace(/\[|\]/g, '');
+			arr.push(attr);
+			selector = selector.replace(aReg, '');
+		}
+		
+		// match tag name
+		var	tag = selector.match(/^[^#. ]+/);
 		if(tag){tag = tag[0];}
+		
+		// match id
+		var id = selector.match(/#([^.# ]+)/);
 		if(id){arr.push('id="' + id.pop() + '"');}
+		
+		// match classes
+		var cls = selector.match(/\.[^.# ]+/g);
 		if(cls){
 			cls = cls.join(' ');
 			cls = cls.replace(/\./g, '');
 			arr.push('class="' + cls + '"');
 		}
-		if(attr){
-			attr = attr.join(' ');
-			attr = attr.replace(/\[|\]/g, '');
-			arr.push(attr);
-		}
 		
-		if(tag.match(single)){
-			isSingle = true;
-		}
+		if(tag.match(single)){isSingle = true;}
 		
         return {
-            open: '<' + tag + (arr.length ? ' ' + arr.join(' ') : '') + (isSingle ? ' />' : '>') + '\n',
-            close: isSingle ? '' : '</' + tag + '>\n'
+            open: '<' + tag + (arr.length ? ' ' + arr.join(' ') : '') + (isSingle ? ' />' : '>'),
+            close: isSingle ? '' : '</' + tag + '>'
         };
 	}
 	
